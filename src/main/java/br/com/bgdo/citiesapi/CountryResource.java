61d7c087951e5d9ddfe6e97c186420c14fd70b21
Brunno Oliveira <brunnogoncalves@gmail.com>
2021-05-09 00:16:21 -0300

    Countries pagination and getById

--- src/main/java/br/com/bgdo/citiesapi/CountryResource.java
+++ src/main/java/br/com/bgdo/citiesapi/CountryResource.java
@@ -2,11 +2,15 @@ package br.com.bgdo.citiesapi;
 
 import br.com.bgdo.citiesapi.countries.Country;
 import br.com.bgdo.citiesapi.repository.CountryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -19,7 +23,17 @@ public class CountryResource {
     }
 
     @GetMapping
-    public List<Country> countries() {
-        return repository.findAll();
+    public Page<Country> countries(Pageable page) {
+        return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Country> getOne(@PathVariable Long id) {
+        Optional<Country> country = repository.findById(id);
+        if(country.isPresent()) {
+            return ResponseEntity.ok().body(country.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
