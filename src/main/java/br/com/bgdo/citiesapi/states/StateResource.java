b7cd2745aa8854351be2ce3508ebf48f8ee56e15
Brunno Oliveira <brunnogoncalves@gmail.com>
2021-05-10 11:38:22 -0300

    Returning ResponseEntity in resources

--- src/main/java/br/com/bgdo/citiesapi/states/StateResource.java
+++ src/main/java/br/com/bgdo/citiesapi/states/StateResource.java
@@ -1,6 +1,8 @@
 package br.com.bgdo.citiesapi.states;
 
 import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,7 @@ public class StateResource {
     }
 
     @GetMapping
-    public List<State> states() {
-        return repository.findAll();
+    public ResponseEntity states() {
+        return ResponseEntity.ok(repository.findAll());
     }
 }
